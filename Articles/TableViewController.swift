1793eb5b2736dc11e89da5e2197fab864ecc1c0a
Volodymyr Magazii <vmagaziy@gmail.com>
2019-11-10 15:04:03 +0100

    Add descriptor for table view cells and use it to setup a stub cell for article teaser

--- Articles/TableViewController.swift
+++ Articles/TableViewController.swift
@@ -1,7 +1,33 @@
 import UIKit
 
+struct TableViewCellDescriptor {
+    let cellClass: UITableViewCell.Type
+    let reuseIdentifier: String
+    let configure: (UITableViewCell) -> Void
+    
+    init<Cell: UITableViewCell>(reuseIdentifier: String, configure: @escaping (Cell) -> Void) {
+        self.cellClass = Cell.self
+        self.reuseIdentifier = reuseIdentifier
+        self.configure = { cell in
+            guard let cell = cell as? Cell else { fatalError() }
+            configure(cell)
+        }
+    }
+}
+
 class TableViewController<Item>: UITableViewController {
+    private let cellDescriptor: (Item) -> TableViewCellDescriptor
+
     var didSelect: (Item) -> Void = { _ in }
+    
+    init(style: UITableView.Style, cellDescriptor: @escaping (Item) -> TableViewCellDescriptor) {
+        self.cellDescriptor = cellDescriptor
+        super.init(style: style)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
