bc588028c0994729ff19fa9dd39644ea8810da9b
Vova Magazii <vova@schibsted.com>
2019-11-10 15:05:45 +0100

    Added entity to abstract a data source of table view and start returning number items and cells

--- Articles/TableViewController.swift
+++ Articles/TableViewController.swift
@@ -15,8 +15,14 @@ struct TableViewCellDescriptor {
     }
 }
 
+enum TableViewSource<Item> {
+    case unknown
+    case items([Item])
+}
+
 class TableViewController<Item>: UITableViewController {
     private let cellDescriptor: (Item) -> TableViewCellDescriptor
+    private var reuseIdentifiers: Set<String> = []
 
     var didSelect: (Item) -> Void = { _ in }
     
@@ -29,10 +35,57 @@ class TableViewController<Item>: UITableViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    var source: TableViewSource<Item> = .unknown {
+        didSet {
+            guard isViewLoaded else { return }
+            
+            switch source {
+            case .unknown:
+                fatalError("Invalid transition to unknown state")
+            case .items:
+                // TODO: IMPLEMENTATION GOES HERE
+                break
+            }
+            
+            tableView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 44
         tableView.cellLayoutMarginsFollowReadableWidth = true
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch source {
+        case .unknown: return 0
+        case .items(let items): return items.count
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let descriptor = cellDescriptor(item(for: indexPath))
+        
+        if !reuseIdentifiers.contains(descriptor.reuseIdentifier) {
+            tableView.register(descriptor.cellClass, forCellReuseIdentifier: descriptor.reuseIdentifier)
+            reuseIdentifiers.insert(descriptor.reuseIdentifier)
+        }
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: descriptor.reuseIdentifier, for: indexPath)
+        
+        descriptor.configure(cell)
+        
+        return cell
+    }
+    
+    private func item(for indexPath: IndexPath) -> Item {
+        switch source {
+        case .unknown:
+            fatalError("Not to be requested for this state")
+        case .items(let items):
+            return items[indexPath.row]
+        }
     }
 }
