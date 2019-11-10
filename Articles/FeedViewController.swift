a0e241ae91664200cdc018697131c73d519c339f
Vova Magazii <vova@schibsted.com>
2019-11-10 21:16:22 +0100

    Show images for case studies using image fetching. This is done in a custom cell to show them above text.

--- Articles/FeedViewController.swift
+++ Articles/FeedViewController.swift
@@ -50,9 +50,63 @@ final class FeedViewController: TableViewController<ArticleType> {
 }
 
 private class TeaserTableViewCell: UITableViewCell {
+    private static let imageRatio: CGFloat = 16 / 9
+    private static let placholderImage = UIGraphicsImageRenderer(size: CGSize(width: 1, height: 1)).image { _ in
+        UIColor.lightGray.set()
+        UIRectFill(CGRect(x: 0, y: 0, width: 1, height: 1))
+    }
+    private static let textEstimatedHeight: CGFloat = 100
+    
+    private let teaserImageView = UIImageView()
+    private let teaserTextLabel = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        [teaserImageView, teaserTextLabel].forEach(contentView.addSubview)
+        
+        teaserTextLabel.numberOfLines = 0
+        
+        teaserImageView.contentMode = .scaleAspectFill
+        teaserImageView.clipsToBounds = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        let textMargins = contentView.layoutMargins
+        let textWidth = size.width - textMargins.left - textMargins.right
+        let textHeight = teaserTextLabel.attributedText != nil ? teaserTextLabel.sizeThatFits(CGSize(width: textWidth, height: 0)).height : TeaserTableViewCell.textEstimatedHeight
+        
+        let imageHeight = teaserImageView.isHidden ? 0 : bounds.width / TeaserTableViewCell.imageRatio
+        let height = imageHeight + textMargins.top + textHeight + textMargins.bottom
+        return CGSize(width: size.width, height: height)
+    }
+    
+    override func layoutSubviews() {
+        let imageHeight = teaserImageView.isHidden ? 0 : bounds.width / TeaserTableViewCell.imageRatio
+        teaserImageView.frame = CGRect(x: 0, y: 0, width: bounds.width, height: imageHeight)
+        
+        let textBounds = contentView.bounds
+        let textMargins = contentView.layoutMargins
+        let textInsetBounds = textBounds.inset(by: textMargins)
+        let textHeight = ceil(textInsetBounds.height - imageHeight) // rounding needed as text needs a bigger height; apparently this difference is caused by hairline added by UIKit
+        
+        teaserTextLabel.frame = CGRect(x: textInsetBounds.minX, y: textInsetBounds.minY + imageHeight, width: textInsetBounds.width, height: textHeight)
+    }
+        
     func configure(for article: ArticleType) {
-        textLabel?.attributedText = article.attributedText
-        textLabel?.numberOfLines = 0
+        teaserTextLabel.attributedText = article.attributedText
+        if let url = article.image {
+            let viewWidth = contentView.bounds.width
+            let imageHeight = viewWidth / TeaserTableViewCell.imageRatio
+            teaserImageView.fetchThumbnailImage(for: url, size: CGSize(width: viewWidth, height: imageHeight), placeholderImage: TeaserTableViewCell.placholderImage)
+            teaserImageView.isHidden = false
+        } else {
+            teaserImageView.resetImage()
+            teaserImageView.isHidden = true
+        }
     }
 }
 
