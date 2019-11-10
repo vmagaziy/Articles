2721acd3dac7356e8347ba304c0630a344aa73b1
Vova Magazii <vova@schibsted.com>
2019-11-10 15:08:17 +0100

    - Add stub articles to data provider - Add basic data loading in feed view controller

--- Articles/FeedViewController.swift
+++ Articles/FeedViewController.swift
@@ -16,6 +16,15 @@ final class FeedViewController: TableViewController<ArticleType> {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadData()
+    }
+    
+    private func loadData() {
+        source = .items(dataProvider.articles())
+    }
 }
 
 private class TeaserTableViewCell: UITableViewCell {
